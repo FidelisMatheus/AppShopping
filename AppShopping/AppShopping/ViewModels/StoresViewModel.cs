039c9e5151ea7c8e0e9387aa2e7c073f71856c7a
Matheus Fidelis <matheusfidelis@yahoo.com>
2022-01-21 21:38:07 -0300

    Update of StoreViewModel

---
+++ AppShopping/AppShopping/ViewModels/StoresViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppShopping.ViewModels
+{
+    public class StoresViewModel
+    {
+        /* 
+         * MVVM - View <-> ViewModel <-> Model
+         * - Commands: Ações (Events) > Métodos.
+         * - Bindings: Vinculos -> Propriedades.
+         * - Notifications:
+         * 
+         * 
+         * View(Lojas-Stores): Entry(Text=Binding SearchWord, Mode=TwoWay)
+         * ViewMOdel: string SearchWord (é nome da propriedade que será vinculada na View).
+         * Entry-Text=Renner (ViewModel-SearchWord:: (recebendo) Renner).
+         * 
+         * Notificação: INotifyPropertyChanged: Houve uma mudança em uma propriedade. MessageCenter(Code Behind)
+         * 
+         */
+    }
+}
