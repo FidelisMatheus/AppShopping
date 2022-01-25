d3b76f1eedb0b705a25c1c3fd39de1ad03854a77
Matheus Fidelis <matheusfidelis@yahoo.com>
2022-01-25 17:56:36 -0300

    Add .gitIgnore and edited Film Detail
    I was edited film detail creating session group and switch collectionview on FilmDetails View

---
+++ AppShopping/AppShopping/Models/SessionGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppShopping.Models
+{
+    public class SessionGroup : List<String>
+    {
+        public string Name { get; set; }
+
+        //ctor - cria construtor
+        public SessionGroup(string name, List<String> list) : base(list)
+        {
+            Name = name;
+        }
+    }
+}
