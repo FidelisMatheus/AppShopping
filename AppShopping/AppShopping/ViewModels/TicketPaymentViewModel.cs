a87005e833597d3342f9dd5947814150f2d823e0
Matheus Fidelis <matheusfidelis@yahoo.com>
2022-01-27 17:39:06 -0300

    TicketPayment
    Was created ticket payment view, ticket payment view model and edited XAML document

---
+++ AppShopping/AppShopping/ViewModels/TicketPaymentViewModel.cs
@@ -0,0 +1,43 @@
+﻿using AppShopping.Libraries.Helpers.MVVM;
+using AppShopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppShopping.ViewModels
+{
+    [QueryProperty("Number", "number")]
+    public class TicketPaymentViewModel : BaseViewModel
+    {
+        private string _number;
+
+        public String Number
+        {
+            set
+            {
+                _number = value; //aqui é o ticket scaneado
+                OnPropertyChanged(nameof(Number));
+
+                //TODO - Pesquisar Ticket e Jogar na tela.
+            }
+        }
+
+        private Ticket _ticket;
+
+        public Ticket Ticket
+        {
+            get { return _ticket; }
+            set 
+            { 
+                SetProperty(ref _ticket, value);
+            }
+        }
+
+
+        public TicketPaymentViewModel()
+        {
+
+        }
+    }
+}
