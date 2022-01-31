851e42665e401580e3f4a69a22d6b655ae2b90d7
Matheus Fidelis <matheusfidelis@yahoo.com>
2022-01-31 17:37:11 -0300

    Payment update
    Payment method and model of credit card created and edited.
    Payment Service Created

---
+++ AppShopping/AppShopping/Services/PaymentService.cs
@@ -0,0 +1,20 @@
+﻿using AppShopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppShopping.Services
+{
+    public class PaymentService
+    {
+        public int SendPayment(CreditCard creditCard) //realiza o pagamento
+        {
+            if(creditCard.SecurityCode == "111")
+            {
+                throw new Exception("Código de segurança inválido!");
+            }
+
+            return 1; //geralmente enviaria um codigo de transação
+        }
+    }
+}
--- AppShopping/AppShopping/ViewModels/TicketPaymentViewModel.cs
+++ AppShopping/AppShopping/ViewModels/TicketPaymentViewModel.cs
@@ -4,6 +4,7 @@ using AppShopping.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace AppShopping.ViewModels
@@ -21,7 +22,7 @@ namespace AppShopping.ViewModels
                 //Pesquisar Ticket e Jogar na tela.
                 Ticket = _ticketService.GetTicketInfo(value);
 
-                //TODO - Atribuir Data final e Calcular o valor do Ticket.
+                //GetTicketInfo já realiza o calculo necessario.
             }
         }
 
@@ -35,10 +36,51 @@ namespace AppShopping.ViewModels
             }
         }
 
+        private CreditCard _creditCard;
+
+        public CreditCard CreditCard
+        {
+            get { return _creditCard; }
+            set 
+            { 
+                SetProperty(ref _creditCard, value);
+            }
+        }
+
+        public ICommand PaymentCommand { get; set; } //Botao de pagamento
+        private PaymentService _paymentService;
+
+
         private TicketService _ticketService;
         public TicketPaymentViewModel()
         {
             _ticketService = new TicketService();
+            _paymentService = new PaymentService();
+
+            CreditCard = new CreditCard();
+
+            PaymentCommand = new Command(Payment);
+        }
+
+        private void Payment()
+        {
+            //TODO - Validações - Manual, Data Annotations e Fluent Validation (pesquisar)
+
+            try //Integração com um Serviço API.
+            {
+                int paymentId = _paymentService.SendPayment(CreditCard);
+
+                //TODO - Redirecionar para tela de sucesso.
+
+            }
+            catch (Exception)
+            {
+
+                //TODO - Colocar mensagem de erro. redirecionar erro
+                throw;
+            }
+
+
         }
     }
 }
